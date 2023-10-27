1526fba6923dca7da1fae4e71898826a471a9911
Mauricio Blanco <mauriblanco.s@gmail.com>
2023-10-27 01:47:25 -0300

    fix: file html loaded in server

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-const container = document.querySelector(".particle-container");
-const colors = ["#439bf6", "#fc45e0"];
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".particle-container");
+  const colors = ["#439bf6", "#fc45e0"];
 
-function createParticle() {
-  const particle = document.createElement("div");
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  function createParticle() {
+    const particle = document.createElement("div");
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-  particle.className = "particle";
-  particle.style.backgroundColor = randomColor;
-  particle.style.boxShadow = `0 0 1rem ${randomColor}, 0 0 1.5rem ${randomColor}`;
+    particle.className = "particle";
+    particle.style.backgroundColor = randomColor;
+    particle.style.boxShadow = `0 0 1rem ${randomColor}, 0 0 1.5rem ${randomColor}`;
 
-  const size = Math.random() * 30 + 10;
-  particle.style.width = `${size}px`;
-  particle.style.height = `${size}px`;
+    const size = Math.random() * 30 + 10;
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
 
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
-  particle.style.transform = `translate(${x}px, ${y}px)`;
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    particle.style.transform = `translate(${x}px, ${y}px)`;
 
-  container.appendChild(particle);
+    container.appendChild(particle);
 
-  requestAnimationFrame(() => {
-    particle.style.transform = "translate(0, 0)";
-    particle.style.opacity = 1;
-  });
-
-  setTimeout(() => {
-    particle.style.opacity = 0;
     requestAnimationFrame(() => {
-      container.removeChild(particle);
+      particle.style.transform = "translate(0, 0)";
+      particle.style.opacity = 1;
     });
-  }, 1000);
-}
 
-setInterval(createParticle, 70);
+    setTimeout(() => {
+      particle.style.opacity = 0;
+      requestAnimationFrame(() => {
+        container.removeChild(particle);
+      });
+    }, 1000);
+  }
+
+  setInterval(createParticle, 70);
+});
